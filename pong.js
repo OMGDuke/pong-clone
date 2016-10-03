69a0e8e113b65a9be4e1d9f96452544cf3ab1a74
OMGDuke <lex1989@gmail.com>
2016-10-03 16:24:34 +0100

    Added paddle collsions

--- pong.js
+++ pong.js
@@ -22,7 +22,7 @@ var step = function() {
 }
 
 var update = function() {
-  ball.update();
+  ball.update(player.paddle, computer.paddle);
 };
 
 var player = new Player();
@@ -82,7 +82,42 @@ Ball.prototype.render = function() {
   context.fill();
 };
 
-Ball.prototype.update = function() {
+Ball.prototype.update = function(paddle1, paddle2) {
   this.x += this.x_speed;
   this.y += this.y_speed;
+  var top_x = this.x - 5;
+  var top_y = this.y - 5;
+  var bottom_x = this.x + 5;
+  var bottom_y = this.y + 5;
+
+  if(this.x - 5 < 0) { // colliding with the left wall
+    this.x = 5;
+    this.x_speed = - this.x_speed;;
+  } else if(this.x + 5 > 400) { //colliding with the right wall
+    this.x = 395;
+    this.x_speed = -this.x_speed;
+  }
+
+  if(this.y < 0 || this.y > 600) { // point scored
+    this.x_speed = 0;
+    this.y_speed = 3;
+    this.x = 200;
+    this.y = 300;
+  }
+
+  if(top_y > 300) {
+    if(top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle1.x) {
+      // collding with the players paddle
+      this.y_speed = -3;
+      this.x_speed += (paddle2.x_speed / 2);
+      this.y += this.y_speed;
+    }
+  } else {
+    if(top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
+      // collding with the computers paddle
+      this.y_speed = 3;
+      this.x_speed += (paddle2.x_speed / 2);
+      this.y += this.y_speed;
+    }
+  }
 };
